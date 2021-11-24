13ef498dcebc19091774f94e4e88ef7ecea3df1e
Shaik abrar <78590172+devShaik010@users.noreply.github.com>
2021-11-23 20:32:41 -0800

    find odd and even
    this program for find , odd and even number ..

---
+++ find.py
@@ -0,0 +1,8 @@
+#  this program for find odd and even numbers 
+val1=int(input("ENTER NUMBER : "))
+ 
+#   program to find add or even 
+if (val1%2)==0:
+    print(val1 ,"IS EVEN NUMBER")
+else:
+    print( val1 ,"IS ODD NUMBER")    
