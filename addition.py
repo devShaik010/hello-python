2f572850628c97fb8ec2651717e85157be00f101
Shaik abrar <78590172+devShaik010@users.noreply.github.com>
2021-11-23 19:37:04 -0800

    python .....
    this is python program for add two value ...

---
+++ addition.py
@@ -0,0 +1,6 @@
+  # adding two numbers code in python.
+user1=input("ENTER FIRST   NUMBER : ")
+user2=input("ENTER SECONDE NUMBER : ")
+ 
+  #   program to add value. 
+print("SUM OF TWO NUMBER IS :",int(user1)+int(user2))
