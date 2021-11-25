cf763e42e33919835a8a3992006898f28e952470
Shaik abrar <78590172+devShaik010@users.noreply.github.com>
2021-11-24 21:22:56 -0800

    Find data type of variable
    There is way to find variable data type , u can use the program to find data type of the given variable ,

---
+++ type.py
@@ -0,0 +1,17 @@
+# program to find variable  type in python 
+from typing import Type
+
+
+val1= "devshaik010"
+val2= 1010
+val3= 1j
+
+#  output is whta is data type
+print("this is",type(val1))
+print("this is ",type(val2))
+print("this is",type(val3))
+print("str ==> string")
+print("int ==> integer")
+print("float ==> float")
+print("bool ==> bollion")
+print("complex = colmplex")
